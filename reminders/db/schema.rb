21b5f97ef46152c1fba2b982b245f2e586dcd0ee
zchi88 <zhihaochi@gmail.com>
2015-11-15 22:37:33 -0500

    Updated reminder to be associated to a user

--- reminders/db/schema.rb
+++ reminders/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116021136) do
+ActiveRecord::Schema.define(version: 20151116031017) do
+
+  create_table "reminders", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reminders", ["user_id"], name: "index_reminders_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
