c5179f224cc7c734c0c9228a2faa9da9c0e1cce8
zchi88 <zhihaochi@gmail.com>
2015-11-15 21:10:14 -0500

    Devise setup

--- reminders/config/environments/development.rb
+++ reminders/config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+  # Configure mailer to send test emails for development
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
