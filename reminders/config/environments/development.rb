d3e5ae224fb54852ce8dee8cd4fde196b8e8303d
zchi88 <zhihaochi@gmail.com>
2015-11-17 00:46:24 -0500

    Configuring email account validation for future use

--- reminders/config/environments/development.rb
+++ reminders/config/environments/development.rb
@@ -37,5 +37,6 @@ Rails.application.configure do
 
 
   # Configure mailer to send test emails for development
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
