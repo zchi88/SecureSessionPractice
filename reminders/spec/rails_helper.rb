804a8f911c435b5efceda4fede30478692521d72
zchi88 <zhihaochi@gmail.com>
2015-12-20 17:43:26 -0500

    Including devise test helper methods

--- reminders/spec/rails_helper.rb
+++ reminders/spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'devise'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -52,4 +54,5 @@ RSpec.configure do |config|
 
   # Allows us to call FactoryGirl methods without prefacing it with "FactoryGirl"
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, :type => :controller
 end
